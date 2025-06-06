1d596bfb5f8473a5ccb333f5424683086ab33d84
Benyamin Eskandari <me@benyaamin.com>
2025-06-06 15:51:56 +0330

    feat: Add shouldStartAnotherExt to DownloadStatus

--- rakhsh/src/main/java/com/benyaamin/rakhsh/model/DownloadStatus.kt
+++ rakhsh/src/main/java/com/benyaamin/rakhsh/model/DownloadStatus.kt
@@ -12,3 +12,7 @@ enum class DownloadStatus {
 fun DownloadStatus.shouldRemoveFromOngoing(): Boolean {
     return this == DownloadStatus.Completed || this == DownloadStatus.Error || this == DownloadStatus.Stopped
 }
+
+fun DownloadStatus.shouldStartAnother(): Boolean {
+    return this == DownloadStatus.Completed || this == DownloadStatus.Error || this == DownloadStatus.Paused || this == DownloadStatus.Stopped
+}
